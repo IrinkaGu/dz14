7b3a0d49162738619ae2973ed9903f11df221c5b
IrinkaGu <gusai2016@yandex.ru>
2017-05-19 21:51:25 +0300

    loader

---
+++ app/src/PokemonList/PokemonListCtrl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+pokemonApp.controller('PokemonListCtrl', function($scope, PokemonsService, BerriesService) {
+
+    // PokemonsService.getPokemons().then(function(response) {
+    //     $scope.pokemons = response.data.results;
+    // });
+    //
+    // BerriesService.getBerries().then(function(response) {
+    //     $scope.berries = response.data.results;
+    // });
+	$scope.loading = 'loading';
+	
+    PokemonsService.getPokemons().then(function(response) {
+        $scope.pokemons = response.data.results;
+
+        return BerriesService.getBerries()
+    }).then(function(response) {
+        $scope.berries = response.data.results;
+		$scope.loading = 'ready';
+    });
+
+
+
+});
